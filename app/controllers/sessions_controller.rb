ae9038c9a4ab5d430d2bd97315b683bfc7feec27
Ivan <ivan.akastolov@gmail.com>
2024-03-29 21:45:54 -0400

    added updated and delete for users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: email)
 
     if user&.authenticate(password)
+      forwarding_url = session[:forwarding_url]
       reset_session
       log_in user
 
@@ -17,7 +18,7 @@ class SessionsController < ApplicationController
         forget user
       end
 
-      redirect_to user
+      redirect_to forwarding_url || user
     else
       flash.now[:danger] = "Incorrect login / password!"
       render "new", status: :unauthorized
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,35 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
   def show
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    flash[:success] = "#{@user.name} was deleted"
+    @user.destroy!
+    redirect_to users_url
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render "edit", status: :unprocessable_entity
+    end
   end
 
   def new
@@ -24,5 +53,22 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url, status: :see_other
+    end
+  end
+
+  def correct_user
+    redirect_to root_url, status: :see_other unless params[:id].to_i == current_user.id
+  end
+
+  def admin_user
+    redirect_to root_url, status: :see_other unless current_user.admin?
   end
 end
