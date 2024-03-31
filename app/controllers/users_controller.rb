05443044be7bc44b6d345821c9a56fd133485226
Ivan <ivan.akastolov@gmail.com>
2024-03-31 13:14:37 -0400

    added microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user, only: [:destroy]
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     redirect_to root_url, status: :see_other unless @user.activated
   end
 
@@ -53,15 +54,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url, status: :see_other
-    end
   end
 
   def correct_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
-  get 'account_activations/edit'
+  get "password_resets/new"
+  get "password_resets/edit"
+  get "account_activations/edit"
   root "static_pages#home"
 
   get "/help", to: "static_pages#help"
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
+  get "/microposts", to: "static_pages#home"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
