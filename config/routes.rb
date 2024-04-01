970958ad9fdeb8254a374b0febda3d17e5601c3f
Ivan <ivan.akastolov@gmail.com>
2024-04-01 00:22:58 -0400

    added feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   get "password_resets/new"
   get "password_resets/edit"
   get "account_activations/edit"
-  root "static_pages#home"
 
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
@@ -13,10 +13,15 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   get "/microposts", to: "static_pages#home"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
